9e5874033d062673cdd4e5eb7737f8c3e0cf6bac
unknown <developerlaju@gmail.com>
2023-05-14 16:15:35 +0600

    Convert All HTML File to PHP File

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+  //include header.php
+  include('header.php')
+  
+?>
+<?php 
+  //include banner.php
+  include('Template/banner.php')
+  
+?>
+
+<?php 
+  //include top-sale.php
+  include('Template/top-sale.php')
+  
+?>
+
+<?php 
+  //include special-price.php
+  include('Template/special-price.php')
+  
+?>
+
+<?php 
+  //include banner-ads.php
+  include('Template/banner-ads.php')
+  
+?>
+
+<?php 
+  //include new-phons.php
+  include('Template/new-phons.php')
+  
+?>
+
+<?php 
+  //include blog.php
+  include('Template/blogs.php')
+  
+?>
+
+<?php 
+//include footer.php
+include('footer.php')
+?>
---
+++ product.php
@@ -0,0 +1,23 @@
+<?php 
+  //include header.php
+  include('header.php')
+  
+?>
+
+<?php 
+  //include top-sale.php
+  include('Template/products.php')
+  
+?>
+
+<?php 
+  //include top-sale.php
+  include('Template/top-sale.php')
+  
+?>
+
+
+<?php 
+//include footer.php
+include('footer.php')
+?>
